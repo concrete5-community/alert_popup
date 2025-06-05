71403a6d69f598daec37494d8cf7337e11e98e86
Michele Locati <michele@locati.it>
2025-06-05 11:03:24 +0200

    Version 1.3.1

--- controller.php
+++ controller.php
@@ -10,7 +10,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'alert_popup';
 
-    protected $pkgVersion = '1.3.0';
+    protected $pkgVersion = '1.3.1';
 
     /**
      * {@inheritdoc}
